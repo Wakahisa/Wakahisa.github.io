9ce3957dfcb9e5b265bab73eff39f4dd74964cac
Wakahisa <zerakyo@gmail.com>
2020-10-05 20:11:16 -0500

    Personalized

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,94 +1,97 @@
 import { nanoid } from 'nanoid';
+import pdf from '../images/PeterTrinhResume.pdf';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Peter Trinh', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Portfolio Website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
-  cta: '',
+  name: 'Peter Trinh',
+  subtitle: "I'm a software engineer & minimalist.",
+  cta: 'Learn more',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'I graduated from the University of Alaska Anchorage with a Bachelor of Science in Computer Science with a minor in Computer Systems Engineering.',
+  paragraphTwo: 'Post-graduation I have been employed as a software engineer at a SAAS company and as a system analyst at a non-profit organization.',
+  paragraphThree: 'Outside of work, I am interested in psychology, finance, science, hiking, and minimalism.',
+  resume: pdf, // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'RilkeSchule.jpg',
+    title: 'Field Trip Permission Slip Appplication',
+    info:
+      'A web application that allows teachers to create field trip forms that parents can sign off on.',
+    info2: 'Technologies involved: ASP.NET MVC, C#, SQL Front-end: Bootstrap, HTML / CSS',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: 'https://github.com/Wakahisa/Rilke-Schule-Student-Management-MVC-asp.Net', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
+    img: 'Ionia.jpg',
+    title: 'Equilibria',
+    info:
+      'A website idea created by a client that allows users to see movie reviews by their favorite critics, celebrities, and friends. The website is suppose to act as a nexus point to existing medias such as Youtube, Netflix, Hulu, and Amazon to allow users to view movies from one access point.',
+    info2: 'Technologies involved: .NET Core, C#, LINQ, SQL Front-end: JavaScript, HTML / CSS',
     url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    repo: '', // if no repo, the button will not show up
   },
-  {
-    id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+  // {
+  //   id: nanoid(),
+  //   img: 'Ionia.jpg',
+  //   title: '',
+  //   info: '',
+  //   info2: '',
+  //   url: '',
+  //   repo: '', // if no repo, the button will not show up
+  // },
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'peterphantrinh@gmail.com',
 };
 
 // FOOTER DATA
 export const footerData = {
   networks: [
-    {
-      id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
+    // {
+    //   id: nanoid(),
+    //   name: 'twitter',
+    //   url: '',
+    // },
+    // {
+    //   id: nanoid(),
+    //   name: 'codepen',
+    //   url: '',
+    // },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/peterptrinh/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/Wakahisa',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
